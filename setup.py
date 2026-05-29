baf740633722a78a940b3462e8be834f43ee540c
larry <larry@source1.jp>
2026-05-29 11:40:54 +0900

    Release lets_be_rational 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='lets_be_rational',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     url='http://jaeckel.org',
     license='MIT',
@@ -13,6 +13,6 @@ setup(
     install_requires = [
         'cody-special>=1.0.0,<2.0.0',
         'piecewise-rational>=1.0.0,<2.0.0',
-        'numpy'
+        'numpy>=1.20'
     ]
 )
